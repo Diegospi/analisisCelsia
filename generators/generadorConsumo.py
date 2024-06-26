7a8b5ecfd8f887f3c2d2ae69d421cc770841c6f2
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-06-26 17:31:02 -0500

    se crea analisis

---
+++ generators/generadorConsumo.py
@@ -0,0 +1,17 @@
+import random
+def generarDatos():
+    datos = []
+    for i in range(5000):
+        dato={}
+        id=random.randint(1,10000)
+        referencia=random.choice(["ACH01","ACH22","ACH43"])
+        marca=random.choice(["Sony","Rico","Kalley"])
+        capacidad=random.randint(100,2000)
+        ciudad=random.choice(["Medellin","Bello","Itagui","Sabaneta","Niquia"])
+        responsable=random.choice(["Mauro Yepes","Wilson Yepes","Santiago Yepes","Sonia Yepes","Tattiana Yepes"])
+
+        dato=[id,referencia,marca,capacidad,ciudad,responsable]
+        datos.append(dato)
+    return datos
+
+print(generarDatos())
