43e3e59146c2f05e6180ea4fe8bf416c1b57214e
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-06-27 17:19:34 -0500

    Se actualiza

--- generators/generadorConsumo.py
+++ generators/generadorConsumo.py
@@ -5,9 +5,9 @@ def generarDatos():
         dato={}
         id=random.randint(1,10000)
         referencia=random.choice(["ACH01","ACH22","ACH43"])
-        marca=random.choice(["Sony","Rico","Kalley"])
+        marca=random.choice(["Sony","Rico","Kalley","-"])
         capacidad=random.randint(100,2000)
-        ciudad=random.choice(["Medellin","Bello","Itagui","Sabaneta","Niquia"])
+        ciudad=random.choice(["Medellin","Bello","Itagui","Sabaneta","Niquia","sin","nan"])
         responsable=random.choice(["Mauro Yepes","Wilson Yepes","Santiago Yepes","Sonia Yepes","Tattiana Yepes"])
 
         dato=[id,referencia,marca,capacidad,ciudad,responsable]
